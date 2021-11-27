1c9c96890b645e5912c0eb82c52aa7e0fe1bf50e
LasithEranga <lasitheranga1@gmail.com>
2021-11-27 17:15:45 +0530

    Rule section

---
+++ Settings/get_rule_details.php
@@ -0,0 +1,19 @@
+<?php
+include('../db.php');
+$query = "";
+$html_code = "";
+$query = "SELECT * FROM `rule`";
+$result = mysqli_query($conn, $query);
+while ($data = mysqli_fetch_array($result)) {
+$html_code .= "<tr>
+                    <td>" . $data['rule_id'] . "</td>
+                    <td>" . $data['rule_name'] . "</td>
+                    <td>" . $data['description'] . "</td>
+                    <td>" . $data['penalty_amount'] . "</td>
+                    <td>" . $data['tag'] . "</td>
+                    <td onclick=showUpdateModal('".$data['rule_id']."','driver')><i class='fas fa-user-edit'></i></td>
+                </tr>";
+}
+
+
+echo $html_code;
--- Settings/users.php
+++ Settings/users.php
@@ -1,29 +1,31 @@
- <!-- add police officer modal -->
+ <!-- add/update user modal -->
  <div class="modal fade" id="nic_input" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
      <div class="modal-dialog  modal-dialog-centered">
-         <div class="modal-content">
+         <div class="modal-content bg-dark text-light">
              <div class="modal-header">
-                 <h5 class="modal-title" id="staticBackdropLabel">Insert Driver Details</h5>
-                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                 <h5 class="modal-title" id="add_update_heading">Insert Driver Details</h5>
+                 <button type="button" class="btn text-light" data-bs-dismiss="modal"> <i class="fas fa-times fs-5"></i></button>
+
+                 <!-- <button type="button" class="btn-close text-light" data-bs-dismiss="modal" aria-label="Close"></button> -->
              </div>
-             <div class="modal-body">
+             <div class="modal-body ">
                  <form id="modal_items" action="add_driver.php" method="post">
-                     
+
                  </form>
              </div>
              <div class="modal-footer " style="justify-content:space-around">
-                     <button type="button" class="btn btn-primary col-3" data-bs-dismiss="modal">Cancel</button>
-                     <button type="button" class="btn btn-primary col-3 mx-3">Clear All</button>
-                     <button id="save" type="button" class="btn btn-primary col-3 ">Okay</button>
+                 <button type="button" class="btn btn-danger col-3" data-bs-dismiss="modal">Cancel</button>
+                 <button type="button" class="btn btn-primary col-3 mx-3">Clear All</button>
+                 <button id="save" type="button" class="btn btn-success col-3 ">Save</button>
              </div>
          </div>
      </div>
  </div>
- <button type="button" id="add_driver_modal" hidden="true" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#nic_input">
+ <button type="button" id="add_user_modal" hidden="true" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#nic_input">
  </button>
- <!-- add police Officer modal end-->
+ <!-- add/update user modal end-->
 
- <!-- Modal -->
+ <!--messagebox Modal -->
  <div class="modal fade" id="messageBox" tabindex="-1">
      <div class="modal-dialog modal-dialog-centered">
          <div class="modal-content">
@@ -55,7 +57,6 @@
              <div class="collapse navbar-collapse" id="navbarScroll">
                  <input class="form-control me-2 bg-dark text-light border_date_input" type="search" placeholder="Search" aria-label="Search">
                  <button class="btn btn-success me-3 " type="submit">Search</button>
-
              </div>
          </div>
      </nav>
@@ -86,7 +87,7 @@
 
              <button type="button" class="btn  btn-block btn-success px-4 mb-2 mb-md-0 me-md-2">Share</button>
              <button type="button" class="btn  btn-block btn-success px-4 me-md-2">Save as PDF</button>
-             <button type="button" class="btn  btn-block btn-success px-4 me-md-2">Add User</button>
+             <button id="btn_add_user" type="button" class="btn  btn-block btn-success px-4 me-md-2" onclick="showAddUserModal()">Add User</button>
 
          </div>
 
@@ -105,10 +106,15 @@
      const drivers = document.getElementById('drivers');
      const table_contents = document.getElementById('table_contents');
      const modal_items = document.getElementById('modal_items');
-     const add_driver_modal = document.getElementById('add_driver_modal');
-     
-     var selected_section = "all_users"
+     const add_user_modal = document.getElementById('add_user_modal');
+     const btn_add_user = document.getElementById('btn_add_user');
+     const add_update_heading = document.getElementById('add_update_heading');
 
+     var selected_section = "all_users"
+     //initially loads to all user, therefore add user button is hidden
+     btn_add_user.hidden = true;
+
+     //removes green line and hover effect from given elements
      function removeLine() {
          all_users.classList.remove('bottom-green', 'cursor_change');
          officers.classList.remove('bottom-green', 'cursor_change');
@@ -118,6 +124,7 @@
          drivers.classList.add('cursor_change');
      }
 
+     //adds the green line and hover effet to section element
      function addLine(section) {
          removeLine();
          section.classList.add('bottom-green');
@@ -128,18 +135,21 @@
          addLine(all_users);
          selected_section = "all_users";
          fillTable();
+         btn_add_user.hidden = true;
      });
 
      officers.addEventListener('click', () => {
          addLine(officers);
          selected_section = "officers";
          fillTable();
+         btn_add_user.hidden = false;
      });
 
      drivers.addEventListener('click', () => {
          addLine(drivers);
          selected_section = "drivers";
          fillTable();
+         btn_add_user.hidden = false;
      });
 
      function showMsg(title, body) {
@@ -149,14 +159,15 @@
      }
 
 
-     //fills table with data available in db for selected date range
+
+     //fills table with data available in db 
      function fillTable() {
          const http_req = new XMLHttpRequest();
          http_req.onload = function() {
              // console.log(this.responseText);
              table_contents.innerHTML = this.responseText;
              if (this.responseText == "") {
-                 showMsg("Data not found!", "Sorry! No data available in seleceted date range");
+                 showMsg("Data not found!", "Sorry! No data available");
              }
          }
          http_req.open('GET', "Settings/get_users_details.php?selected_section=" + selected_section);
@@ -168,12 +179,101 @@
      function showUpdateModal(user_id, user_type) {
          const http_req = new XMLHttpRequest();
          http_req.onload = function() {
-            // console.log(this.responseText);
-            modal_items.innerHTML = this.responseText;
-            add_driver_modal.click();
+             // console.log(this.responseText);
+             modal_items.innerHTML = this.responseText;
+             if(selected_section == "officers"){
+                add_update_heading.innerHTML = "Police Officer Details";
              
+             }
+             else if(selected_section == "drivers"){
+                add_update_heading.innerHTML = "Driver Details"
+             }
+             add_update_heading.classList.add('col', 'text-center');
+             add_user_modal.click();
+
          }
-         http_req.open('GET', "Settings/user_modal_details.php?user_id=" + user_id +"&user_type="+user_type);
+         http_req.open('GET', "Settings/user_modal_details.php?user_id=" + user_id + "&user_type=" + user_type);
          http_req.send();
+     }
+
+     //get data from database for update modal and show the modal
+     function showAddUserModal() {
+         if (selected_section == "drivers") {
+             add_update_heading.innerHTML = "Driver Details";
+             add_update_heading.classList.add('col', 'text-center');
+             modal_items.innerHTML = `<div class='mb-3'>
+                                        <label for='nic' class='form-label'> NIC Number</label>
+                                        <input type='text' class='form-control bg-dark text-light' id='nic' name='nic' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='fname' class='form-label'>First Name</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='fname' name='fname'>
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='lname' class='form-label'>Last Name</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='lname' name='lname' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='fullname' class='form-label'>Full Name</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='fullname' name='fullname' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='address' class='form-label'>Address</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='address' name='address' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='text' class='form-label'>Email Address</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='email' name='email'  >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='phone' class='form-label'>Contact No</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='phone' name='phone' >
+                                    </div>
+                                    <input id='submit' type='submit' hidden='true'>`;
+             add_user_modal.click();
+
+         }
+         if (selected_section == "officers") {
+             add_update_heading.innerHTML = "Police Officer Details"
+             add_update_heading.classList.add('col', 'text-center');
+             modal_items.innerHTML = `<div class='mb-3'>
+                                        <label for='police_id' class='form-label'> Police ID </label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='police_id' name='police_id' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='fname' class='form-label'>First Name</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='fname' name='fname' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='lname' class='form-label'>Last Name</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='lname' name='lname' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='fullname' class='form-label'>Full Name</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='fullname' name='fullname' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='address' class='form-label'>Address</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='address' name='address'>
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='text' class='form-label'>Email Address</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='email' name='email'  >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='nic' class='form-label'> NIC Number</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='nic' name='nic' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='post' class='form-label'> Post</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='post' name='post' >
+                                    </div>
+                                    <div class='mb-3'>
+                                        <label for='phone' class='form-label'>Contact No</label>
+                                        <input type='text' class='form-control bg-dark bg-dark text-light' id='phone' name='phone'>
+                                    </div>
+                                    <input id='submit' type='submit' hidden='true'>`;
+             add_user_modal.click();
+         }
      }
  </script>
