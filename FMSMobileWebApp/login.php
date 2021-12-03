f06a56e501ff262085249a9f4dc4b618e3425c2e
LasithEranga <lasitheranga1@gmail.com>
2021-12-03 09:38:02 +0530

    Final

--- FMSMobileWebApp/login.php
+++ FMSMobileWebApp/login.php
@@ -19,6 +19,7 @@ session_start();
   <link rel="stylesheet" href="landing.css" />
   <!-- Javascripts -->
   <script src="https://cdn.jsdelivr.net/npm/chart.js@3.5.0/dist/chart.min.js"></script>
+  <script src="md5.min.js"></script>
   <style>
     .m-50 {
       margin-top: 50px;
@@ -31,37 +32,102 @@ session_start();
     <div class="m-50 ms-3 fs-3 text-md-center">
       <img src="./images/logo.png" width="120px" alt="" />
     </div>
-    <div class="m-50 ms-3 fs-3">LOGIN</div>
+    <div id="login_label" class="m-50 ms-3 fs-3">LOGIN</div>
     <div class="ms-3 mb-4">Please sign in to continue</div>
     <div class="col-12 d-flex flex-column ms-3">
       <div class="mb-3 col-11">
         <label for="police_id" class="form-label">Username</label>
-        <input type="text" class="form-control" id="police_id" value="45564" placeholder="Eg: -45564" />
+        <input type="text" class="form-control" id="police_id" value="45564" placeholder="45564" />
+      </div>
+      <div class="mb-3 col-11" id="show_new_password" style="display: none;">
+        <label for="new_password" class="form-label" >New Password</label>
+        <input type="password" class="form-control" id="new_password"  placeholder="******" />
       </div>
       <div class="mb-3 col-11">
-        <label for="password" class="form-label">Password</label>
+        <label for="password" id="password_label" class="form-label">Password</label>
         <input type="password" class="form-control" id="password" value="sineru@gmail.com" placeholder="******" />
       </div>
-      <button type="button" onclick="verifyUser()" class="btn btn-primary col-11 mt-5 mb-5">
+      <div id="login_failed_alert" class=" flex-row pt-1 col-11" style="display: none;">
+        <div class="col text-danger">Login Failed ! </div>
+        <div class="col text-end"><a href="#">Forgot password?</a></div>
+      </div>
+      <div id="reset_success" class=" flex-row pt-1" style="display: none;">
+        <div class="col text-success">Password updated! please login using new password</div>
+      </div>
+      <button type="button" id="login_button" onclick="verifyUser()" class="btn btn-primary col-11 mt-5 mb-5">
         Login
       </button>
     </div>
   </div>
 
   <script>
+    
+    const police_id = document.getElementById('police_id');
+    const password = document.getElementById('password');
+    const login_failed_alert = document.getElementById('login_failed_alert');
+    const password_label = document.getElementById('password_label');
+    const show_new_password = document.getElementById('show_new_password');
+    const login_button = document.getElementById('login_button');
+    const login_label = document.getElementById('login_label');
+
+    function validateEmail(value) {
+      //validate email
+      var checkEmail = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:\s@]+)*)|(.+))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (checkEmail.test(value)) {
+        return true;
+      } else {
+        return false;
+      }
+
+    }
+
     function verifyUser() {
-      username = document.getElementById("police_id").value;
-      password = document.getElementById("password").value;
+
+      if (validateEmail(password.value)) {
+        //if the password looks like email, user will be return to reset password section
+        hashPassword = password.value;
+      } else {
+        hashPassword = md5(password.value);
+      }
       const http_req = new XMLHttpRequest();
       http_req.onload = function() {
-        alert(this.responseText);
+        //alert(this.responseText);
         if (this.responseText == "logged") {
-          window.open("home.php", "_self");
+          window.open('home.php', '_self');
+        } else if (this.responseText == "reset") {
+          //shows the password reset section 
+          password.value = "";
+          show_new_password.style.display = "block";
+          password_label.innerHTML = "Verify Password";
+          login_label.innerHTML = "Reset Password";
+          login_button.setAttribute("onClick", "changePassword();")
+          login_button.innerHTML = "Reset";
+
         } else {
-          //document.getElementById("login_failed_alert").classList.add("d-flex");
+          login_failed_alert.classList.add('d-flex');
         }
-      };
-      http_req.open("GET","login_verify.php?police_id=" + username + "&password=" + password);
+
+      }
+      http_req.open('POST', "login_verify.php?police_id=" + police_id.value + "&password=" + hashPassword);
+      http_req.send();
+    }
+
+    function changePassword() {
+      //verify the passwords are same
+      const http_req = new XMLHttpRequest();
+      http_req.onload = function() {
+        if (this.responseText == "success") {
+          //resetting everything and letting user to login with new password
+          password.value = "";
+          login_label.innerHTML = "Login";
+          show_new_password.style.display = "none";
+          password_label.innerHTML = "Password";
+          login_button.innerHTML = "Login";
+          login_button.setAttribute("onClick", "verifyUser();");
+          document.getElementById('reset_success').classList.add('d-flex');
+        }
+      }
+      http_req.open('POST', "password_reset.php?password=" + md5(password.value));
       http_req.send();
     }
   </script>
--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <html>
 <script src="https://smtpjs.com/v3/smtp.js"></script>
 <input type="file" id="fileupload" onchange="uploadFileToServer()" />
+SELECT * FROM `traffic_police_officer` WHERE police_id = '34276' AND email ='lasitheranga1@gmail.com'<br />
+<font size='1'><table class='xdebug-error xe-notice' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Notice: Undefined variable: result_array in C:\wamp64\www\FMS\FMSMobileWebApp\login_verify.php on line <i>36</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0025</td><td bgcolor='#eeeeec' align='right'>410624</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='C:\wamp64\www\FMS\FMSMobileWebApp\login_verify.php' bgcolor='#eeeeec'>...\login_verify.php<b>:</b>0</td></tr>
+</table></font>
+logged
 <script>
 function uploadFileToServer()
 {
